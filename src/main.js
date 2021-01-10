dfa2741775756bd5b0d20b212607f6c7583d7c6d
Edidiong Bassey <edidiong.bassey@frogasia.com>
2021-01-10 21:50:23 +0800

    feat: Specify upload folder and inject multer upload module to call api

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+const multer = require('multer');
+const upload = multer({dest: 'uploads'});
+
 app.get("/", (req, res) => {
     res.sendStatus(200);
 });
 
-app.post("/file-upload", (req, res) => {
+app.post("/file-upload", upload.single('file'), (req, res) => {
     res.sendStatus(200);
 });
 
