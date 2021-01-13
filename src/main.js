edf9259da3391ed515f3af611227a85f1e4c36dd
Edidiong Bassey <edidiong.bassey@frogasia.com>
2021-01-13 12:14:53 +0800

    fix: set absoloute path for uploads directory

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ const imageFilter = (req, file, cb) => {
 // Make file names readable
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads');
+      cb(null, path.join(__dirname,'uploads'))
     },
     filename: function (req, file, cb) {
       // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
